8ac541c9baf73e33c59f2a035e7c29c83ae28d35
adit-ya-dev <aditsingh8077@gmail.com>
2026-01-15 00:55:43 -0800

    complete

---
+++ Math_&_Logic/Add_Digits.py
@@ -0,0 +1,12 @@
+"Given an integer num, repeatedly add its digits until the result has only one digit.
+
+ Example:
+
+Input: num = 38 → Output: 2
+Because: 3+8=11 → 1+1=2"
+
+class Solution:
+    def addDigits(self, num: int) -> int:
+        if num == 0:
+            return 0
+        return 1 + (num - 1) % 9
