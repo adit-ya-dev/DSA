0e4b2d579e0017be569e573a658af29f73b31719
adit-ya-dev <aditsingh8077@gmail.com>
2026-01-17 05:01:44 -0800

    alright

---
+++ practice/Group_Anagrams.py
@@ -0,0 +1,13 @@
+"Input: ["eat","tea","tan","ate","nat","bat"]
+Output: [["eat","tea","ate"],["tan","nat"],["bat"]]"
+
+from collections import defaultdict
+
+def groupAnagrams(strs):
+    mp = defaultdict(list)
+
+    for word in strs:
+        key = ''.join(sorted(word))
+        mp[key].append(word)
+
+    return list(mp.values())
