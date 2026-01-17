40606902d74798fd81b64a109524164eb80a5447
adit-ya-dev <aditsingh8077@gmail.com>
2026-01-17 04:58:57 -0800

    alright

---
+++ practice/Container_With_Most_Water.py
@@ -0,0 +1,17 @@
+"Input: [1,8,6,2,5,4,8,3,7]
+Output: 49"
+
+def maxArea(height):
+    l, r = 0, len(height) - 1
+    best = 0
+
+    while l < r:
+        area = min(height[l], height[r]) * (r - l)
+        best = max(best, area)
+
+        if height[l] < height[r]:
+            l += 1
+        else:
+            r -= 1
+
+    return best
