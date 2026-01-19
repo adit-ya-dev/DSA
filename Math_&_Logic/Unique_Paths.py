9c386d3df145b373891a135fd5d69a996a176c9c
adit-ya-dev <aditsingh8077@gmail.com>
2026-01-19 05:01:42 -0800

    initial commit

---
+++ Math_&_Logic/Unique_Paths.py
@@ -0,0 +1,11 @@
+"Robot moves only right/down in m x n grid. Count paths."
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        N = m + n - 2
+        r = min(m - 1, n - 1)
+
+        ans = 1
+        for i in range(1, r + 1):
+            ans = ans * (N - r + i) // i
+        return ans
