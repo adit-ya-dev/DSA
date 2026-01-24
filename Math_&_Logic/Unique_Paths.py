e3959a285e254c4f0438fb3ea1a2427a93d8cd29
Aditya Pamar <aditsingh8077@gmail.com>
2026-01-24 05:58:51 -0800

    Update Unique_Paths.py

--- Math_&_Logic/Unique_Paths.py
+++ Math_&_Logic/Unique_Paths.py
@@ -4,7 +4,6 @@ class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         N = m + n - 2
         r = min(m - 1, n - 1)
-
         ans = 1
         for i in range(1, r + 1):
             ans = ans * (N - r + i) // i
